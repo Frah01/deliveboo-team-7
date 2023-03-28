7af182c4cca6a675897c265d0b00173bf35966ce
Frah01 <vinario.francesco@gmail.com>
2023-03-28 11:45:51 +0200

    corretto errore parentesi

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
+
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -8,12 +9,12 @@ use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-        public function index(){
+    public function index()
+    {
         $user = Auth::user();
         $user_id = $user->id;
         $restaurant = Restaurant::where('user_id', $user_id)->first();
         $slug = Restaurant::generateSlug($restaurant->nome, '-');
         return view('admin.dashboard', compact('slug'));
-
     }
-
+}
