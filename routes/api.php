7251abf9a8bfdd14dc56de6061a7e76d8c0d6c9b
dunzolo <rossidavide108@gmail.com>
2023-03-29 13:19:02 +0200

    close #82

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\GuestLeadController as GuestLeadController;
-
+use App\Http\Controllers\Api\RestaurantController as RestaurantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/restaurants/{slug}', [RestaurantController::class, 'show']);
 Route::post('/contatti', [GuestLeadController::class, 'store']);
