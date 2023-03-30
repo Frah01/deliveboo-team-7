4654ab5aba35517bdc27a31462654c23122e3757
Frah01 <vinario.francesco@gmail.com>
2023-03-30 12:25:01 +0200

    Aggiunto payment

---
+++ resources/views/payment/success.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+@section('content')
+<div class="container" >
+    <div class="row ">
+        <div class="col ">
+            <h1 class="text-center">Pagamento avvenuto con successo!!!</h1>
+        </div>
+    </div>
+</div>
+@endsection
